37174e9745f5d3bb755505ef68e4061c5a91efd0
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-09-04 13:51:59 +0600

    catch all -segment

---
+++ src/app/categories/[...features]/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const categoriesDetailsPage = ({params}) => {
+    console.log({params});
+    
+    return (
+        <div className='h-screen'>
+            Category Details
+        </div>
+    );
+};
+
+export default categoriesDetailsPage;
